6b87f254df6374b619195c79308997cb267518c2
ianhall351-code <ianhall351@gmail.com>
2026-05-02 23:27:52 -0400

    Add: Contact class

---
+++ src/ContactBook/Contact.cs
@@ -0,0 +1,14 @@
+namespace ContactBook;
+
+public class Contact
+{
+    private string fname;
+    private string lname;
+    private string phone;
+    private string email;
+
+    public Contact(string fname = "", string lname = "", string phone = "", string email = "")
+    {
+
+    }
+}
