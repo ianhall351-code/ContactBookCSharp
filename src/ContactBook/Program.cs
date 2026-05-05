fe83e898d3f0211662d71bd08afc34a5dbc471be
ianhall351-code <ianhall351@gmail.com>
2026-05-05 00:50:06 -0400

    Add: ShowWelcomeScreen

--- src/ContactBook/Program.cs
+++ src/ContactBook/Program.cs
@@ -4,11 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        Contact c1 = new Contact();
-        Contact c2 = new Contact("Henry");
-        Contact c3 = new Contact("Henry", "Bruckman");
-        Contact c4 = new Contact("Henry", "Bruckman", "123-456-7890");
-        Contact c5 = new Contact("Henry", "Bruckman", "123-456-7890", "hbruckman@gmail.com");
-        Contact c6 = new Contact(lname:"Bruckman", email: "hbruckman@gmail.com");
+        var cb = new ContactBook();
+        cb.Start();
     }
 }
